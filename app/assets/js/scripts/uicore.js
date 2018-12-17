3be049c287f1c366b034ee110b5d8711442513be
mindamp <mindamp@gmail.com>
2018-12-17 08:39:19 -0600

    Bleh... automation and stuff.

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -15,8 +15,8 @@ const loggerAutoUpdater        = LoggerUtil('%c[AutoUpdater]', 'color: #000668; 
 const loggerAutoUpdaterSuccess = LoggerUtil('%c[AutoUpdater]', 'color: #209b07; font-weight: bold')
 
 // Log deprecation and process warnings.
-process.traceProcessWarnings = true
-process.traceDeprecation = true
+process.traceProcessWarnings = false
+process.traceDeprecation = false
 
 // Disable eval function.
 // eslint-disable-next-line
@@ -26,9 +26,9 @@ window.eval = global.eval = function () {
 
 // Display warning when devtools window is opened.
 remote.getCurrentWebContents().on('devtools-opened', () => {
-    console.log('%cThe console is dark and full of terrors.', 'color: white; -webkit-text-stroke: 4px #a02d2a; font-size: 60px; font-weight: bold')
-    console.log('%cIf you\'ve been told to paste something here, you\'re being scammed.', 'font-size: 16px')
-    console.log('%cUnless you know exactly what you\'re doing, close this window.', 'font-size: 16px')
+    //console.log('%cThe console is dark and full of terrors.', 'color: white; -webkit-text-stroke: 4px #a02d2a; font-size: 60px; font-weight: bold')
+    //console.log('%cIf you\'ve been told to paste something here, you\'re being scammed.', 'font-size: 16px')
+    //console.log('%cUnless you know exactly what you\'re doing, close this window.', 'font-size: 16px')
 })
 
 // Disable zoom, needed for darwin.
