7089156ed26a3889fcbbdf637d9ce9ae0881a8f9
mindamp <mindamp@gmail.com>
2018-12-17 07:05:12 -0600

    Bleh... automation and stuff.

--- update.sh
+++ update.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-oldVersion=$(grep -m2 "version" app/assets/distribution.json | tail -n1 | awk -F "\"*:\"*" '{print $2}' | sed 's/"//g' |sed 's/\,//g' | sed 's/ //g' | awk -F "\." '{print $3}')
-newVersion=$((oldVersion + 1))
-echo "Old Distribution Version: "$oldVersion
+oldAllParts=$(grep -m2 "version" app/assets/distribution.json | tail -n1 | awk -F "\"*:\"*" '{print $2}' | sed 's/"//g' |sed 's/\,//g' | sed 's/ //g')
+oldFirstPart=$(grep -m2 "version" app/assets/distribution.json | tail -n1 | awk -F "\"*:\"*" '{print $2}' | sed 's/"//g' |sed 's/\,//g' | sed 's/ //g' | awk -F "\." '{print $1}')
+oldSecondPart=$(grep -m2 "version" app/assets/distribution.json | tail -n1 | awk -F "\"*:\"*" '{print $2}' | sed 's/"//g' |sed 's/\,//g' | sed 's/ //g' | awk -F "\." '{print $2}')
+oldThirdPart=$(grep -m2 "version" app/assets/distribution.json | tail -n1 | awk -F "\"*:\"*" '{print $2}' | sed 's/"//g' |sed 's/\,//g' | sed 's/ //g' | awk -F "\." '{print $3}')
+newThirdPart=$((oldThirdPart + 1))
+newAllParts="$oldFirstPart.$oldSecondPart.$newThirdPart"
+echo "Old Minor Distribution Version: "$oldAllParts
 node mods.js;
 jsonlint -i app/assets/distribution.json
-echo "New Distribution Version: "$newVersion
+sed -i 's/$oldAllParts/$newAllParts/g' app/assets/distribution.json
+echo "New Major Distribution Version: "$newAllParts
 bash push.sh;
