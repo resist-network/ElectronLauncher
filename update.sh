e89553f9712ea4819257ae5c355deb5d0676a0c4
mindamp <mindamp@gmail.com>
2018-12-17 06:59:36 -0600

    Bleh... automation and stuff.

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-oldVersion=$(grep -m2 "version" app/assets/distribution.json | tail -n1 | awk -F "\"*:\"*" '{print $2}' | sed 's/"//g' |sed 's/\,//g' | sed 's/ //g')
+oldVersion=$(grep -m2 "version" app/assets/distribution.json | tail -n1 | awk -F "\"*:\"*" '{print $2}' | sed 's/"//g' |sed 's/\,//g' | sed 's/ //g' | awk -F "\." '{print $3}')
 newVersion=$((oldVersion + 1))
 echo "Old Distribution Version: "$oldVersion
 node mods.js;
