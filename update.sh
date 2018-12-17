5d93973a28423924915cca8056b9be6ba7df3ddb
mindamp <mindamp@gmail.com>
2018-12-17 07:12:37 -0600

    Bleh... automation and stuff.

--- update.sh
+++ update.sh
@@ -4,12 +4,12 @@ oldAllParts=$(grep -m2 "version" app/assets/distribution.json | tail -n1 | awk -
 oldFirstPart=$(grep -m2 "version" app/assets/distribution.json | tail -n1 | awk -F "\"*:\"*" '{print $2}' | sed 's/"//g' |sed 's/\,//g' | sed 's/ //g' | awk -F "\." '{print $1}')
 oldSecondPart=$(grep -m2 "version" app/assets/distribution.json | tail -n1 | awk -F "\"*:\"*" '{print $2}' | sed 's/"//g' |sed 's/\,//g' | sed 's/ //g' | awk -F "\." '{print $2}')
 oldThirdPart=$(grep -m2 "version" app/assets/distribution.json | tail -n1 | awk -F "\"*:\"*" '{print $2}' | sed 's/"//g' |sed 's/\,//g' | sed 's/ //g' | awk -F "\." '{print $3}')
-newThirdPart=$((oldThirdPart + 1))
-newAllParts="$oldFirstPart.$oldSecondPart.$newThirdPart"
+oldFourthPart=$(grep -m2 "version" app/assets/distribution.json | tail -n1 | awk -F "\"*:\"*" '{print $2}' | sed 's/"//g' |sed 's/\,//g' | sed 's/ //g' | awk -F "\." '{print $4}')
+newfourthPart=$((oldThirdPart + 1))
+newAllParts="$oldFirstPart.$oldSecondPart.$oldThirdPart.$newFourthPart"
+sed -i "s/$oldAllParts/$newAllParts/g" app/assets/distribution-template.json
 echo "Old Minor Distribution Version: "$oldAllParts
 node mods.js;
 jsonlint -i app/assets/distribution.json
-sed -i "s/$oldAllParts/$newAllParts/g" app/assets/distribution.json
-sed -i "s/$oldAllParts/$newAllParts/g" app/assets/distribution-template.json
 echo "New Major Distribution Version: "$newAllParts
 bash push.sh;
