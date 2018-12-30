097c88debff7a1b518ba3cec9a82dbfc106a87c4
system-operator <mindamp@gmail.com>
2018-12-30 13:18:40 -0600

    Bleh... automation and stuff.

--- mods.js
+++ mods.js
@@ -15,7 +15,7 @@ var thisJSONrequired = '\t\t\t\t'
 var allJSONoptional = ''
 var thisJSONoptional = '\t\t\t\t'
 fs.createReadStream(__dirname+'/app/assets/distribution-template.json').pipe(fs.createWriteStream(__dirname+'/app/assets/distribution.json'))
-dir.files('../mod-pack', function(err, files) {
+dir.files('../resist-mod-pack', function(err, files) {
 	files.forEach(function(file) {
 		var modFile = path.basename(file)
 		var pathSearch = file.toString()
@@ -25,13 +25,13 @@ dir.files('../mod-pack', function(err, files) {
 			var target_type = 'File'
 			var target_size = getFilesizeInBytes(file)
 			var target_md5 = md5File.sync(file)
-			var target_url = downloadCDN+file.toString().replace(/\\/g, '/').replace(/ /g,"%20").replace(/mod-pack\//g,'')
+			var target_url = downloadCDN+file.toString().replace(/\\/g, '/').replace(/ /g,"%20").replace(/resist-mod-pack\//g,'')
 			console.log(fileName+' >> '+target_md5)
 			var target_id = fileName
 			var target_name = fileName
 			console.log(target_url)
 			var ext = path.extname(file)
-			var target_path = file.toString().replace(/\\/g, '/').replace('/config','').replace(/mod-pack\//g,'')
+			var target_path = file.toString().replace(/\\/g, '/').replace('/config','').replace(/resist-mod-pack\//g,'')
 			var thisJSONoptional = '{\r\n\t\t\t"id": "'+target_id+'",\r\n\t\t\t"name": "'+target_name+'",\r\n\t\t\t"type": "'+target_type+'",\r\n\t\t\t\t\t"required": {\r\n\t\t\t\t\t"value": false,\r\n\t\t\t\t\t"def": true\r\n\t\t\t\t},\r\n\t\t\t"artifact": {\r\n\t\t\t\t\t\t"size": '+target_size+',\r\n\t\t\t\t\t"path": "'+target_path+'",\r\n\t\t\t\t\t"MD5": "'+target_md5+'",\r\n\t\t\t\t\t"url": "'+target_url+'"\r\n\t\t\t\t\t}\r\n\t\t\t},'
 			allJSONoptional += thisJSONoptional.toString()
 		} else if(pathSearch.indexOf('\\mods-optional\\') > 0) {
@@ -40,7 +40,7 @@ dir.files('../mod-pack', function(err, files) {
 			var target_size = getFilesizeInBytes(file)
 			var ext = path.extname(file)
 			var target_md5 = md5File.sync(file)
-			var target_url = downloadCDN+file.toString().replace(/\\/g, '/').replace(/ /g,"%20").replace(/mod-pack\//g,'')
+			var target_url = downloadCDN+file.toString().replace(/\\/g, '/').replace(/ /g,"%20").replace(/resist-mod-pack\//g,'')
 			console.log(fileName+' >> '+target_md5)
 			var target_id = 'net.resist:mod:'+target_md5.slice(0, -28)
 			var target_name =fileName
@@ -53,13 +53,13 @@ dir.files('../mod-pack', function(err, files) {
 			var target_type = 'ForgeMod'
 			var target_size = getFilesizeInBytes(file)
 			var target_md5 = md5File.sync(file)
-			var target_url = downloadCDN+file.toString().replace(/\\/g, '/').replace(/ /g,"%20").replace(/mod-pack\//g,'')
+			var target_url = downloadCDN+file.toString().replace(/\\/g, '/').replace(/ /g,"%20").replace(/resist-mod-pack\//g,'')
 			console.log(fileName+' >> '+target_md5)
 			var target_id = 'net.resist:mod:'+target_md5.slice(0, -28)
 			var target_name = fileName
 			console.log(target_url)
 			var ext = path.extname(file)
-			var target_path = file.toString().replace(/\\/g, '/').replace(/mods\//g,'modstore\/').replace(/mod-pack\//g,'')
+			var target_path = file.toString().replace(/\\/g, '/').replace(/mods\//g,'modstore\/').replace(/resist-mod-pack\//g,'')
 			var thisJSONoptional = '{\r\n\t\t\t\t"id": "'+target_id+'",\r\n\t\t\t\t"name": "'+target_name+'",\r\n\t\t"type": "'+target_type+'",\r\n\t\t\t"required": {\r\n\t\t\t\t\t"value": false,\r\n\t\t\t\t\t"def": true\r\n\t\t\t\t},\r\n\t\t\t\t\t"artifact": {\r\n\t\t\t\t"size": '+target_size+',\r\n\t\t\t\t\t"path": "'+target_path+'",\r\n\t\t\t\t\t"MD5": "'+target_md5+'",\r\n\t\t\t\t\t"url": "'+target_url+'"\r\n\t\t\t\t}\r\n\t\t\t},'
 			allJSONoptional += thisJSONoptional.toString()
 		} else if(pathSearch.indexOf('\\config\\') > 0) {
@@ -67,13 +67,13 @@ dir.files('../mod-pack', function(err, files) {
 			var target_type = 'File'
 			var target_size = getFilesizeInBytes(file)
 			var target_md5 = md5File.sync(file)
-			var target_url = downloadCDN+file.toString().replace(/\\/g, '/').replace(/ /g,"%20").replace(/mod-pack\//g,'')
+			var target_url = downloadCDN+file.toString().replace(/\\/g, '/').replace(/ /g,"%20").replace(/resist-mod-pack\//g,'')
 			console.log(fileName+' >> '+target_md5)
 			var target_id = fileName
 			var target_name = fileName
 			console.log(target_url)
 			var ext = path.extname(file)
-			var target_path = file.toString().replace(/\\/g, '/').replace(/mod-pack\//g,'')
+			var target_path = file.toString().replace(/\\/g, '/').replace(/resist-mod-pack\//g,'')
 			var thisJSONoptional = '{\r\n\t\t\t\t\t"id": "'+target_id+'",\r\n\t\t\t\t\t"name": "'+target_name+'",\r\n\t\t\t\t\t"type": "'+target_type+'",\r\n\t\t\t\t\t"required": {\r\n\t\t\t\t\t"value": false,\r\n\t\t\t\t\t"def": true\r\n\t\t\t\t},\r\n\t\t\t\t\t"artifact": {\r\n\t\t\t\t\t"size": '+target_size+',\r\n\t\t\t\t\t"path": "'+target_path+'",\r\n\t\t\t\t\t"MD5": "'+target_md5+'",\r\n\t\t\t\t\t"url": "'+target_url+'"\r\n\t\t\t\t}\r\n\t\t\t},'
 			allJSONoptional += thisJSONoptional.toString()
 		} else if(pathSearch.indexOf('\\resources\\') > 0) {
@@ -81,13 +81,13 @@ dir.files('../mod-pack', function(err, files) {
 			var target_type = 'File'
 			var target_size = getFilesizeInBytes(file)
 			var target_md5 = md5File.sync(file)
-			var target_url = downloadCDN+file.toString().replace(/\\/g, '/').replace(/ /g,"%20").replace(/mod-pack\//g,'')
+			var target_url = downloadCDN+file.toString().replace(/\\/g, '/').replace(/ /g,"%20").replace(/resist-mod-pack\//g,'')
 			console.log(fileName+' >> '+target_md5)
 			var target_id = fileName
 			var target_name = fileName
 			console.log(target_url)
 			var ext = path.extname(file)
-			var target_path = file.toString().replace(/\\/g, '/').replace('/config','').replace(/mod-pack\//g,'')
+			var target_path = file.toString().replace(/\\/g, '/').replace('/config','').replace(/resist-mod-pack\//g,'')
 			var thisJSONoptional = '{\r\n\t\t\t\t"id": "'+target_id+'",\r\n\t\t\t\t"name": "'+target_name+'",\r\n\t\t\t\t\t"type": "'+target_type+'",\r\n\t\t\t\t\t"required": {\r\n\t\t\t\t\t"value": false,\r\n\t\t\t\t\t"def": true\r\n\t\t\t\t},\r\n\t\t\t"artifact": {\r\n\t\t\t\t\t\t"size": '+target_size+',\r\n\t\t\t\t\t"path": "'+target_path+'",\r\n\t\t\t\t\t"MD5": "'+target_md5+'",\r\n\t\t\t\t\t"url": "'+target_url+'"\r\n\t\t\t\t\t}\r\n\t\t\t},'
 			allJSONoptional += thisJSONoptional.toString()
 		}
